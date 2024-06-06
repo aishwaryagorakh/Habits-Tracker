72142c654e02d94af9ba7797c883dd010307c1ab
aishwarya shinde <aishwaryagorakhshinde@gmail.com>
2024-06-06 19:34:37 +0530

    adding new url

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    mongoose.connect("mongodb://127.0.0.1:27017/HabitTracker");
+    mongoose.connect(
+      "mongodb+srv://aishwaryagorakhshinde:WVNim11LygbUXFwJ@cluster2.kh1m9ia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2"
+    );
     console.log("MongoDB connected using mongoose");
   } catch (error) {
     console.log(error);
